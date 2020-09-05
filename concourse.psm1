481c608f132a6ffc788aabc6be7c880a3dc36072
Cansın Aldanmaz <cansinaldanmaz@gmail.com>
2020-09-05 22:07:09 +0300

    Follow rel link removed

--- concourse.psm1
+++ concourse.psm1
@@ -31,7 +31,7 @@ function Invoke-ConcourseAuth {
         Write-Verbose -Message "Filtering url to login for auth type $loginType : $($req.Links.href | Out-String)"
         $newUri = ($req.Content |  Select-String -Pattern  "$linkToLogin\?req\=\w*").Matches.Value
         Write-Verbose -Message "Creating new request to $concourseUrl$newUri" 
-        $req = Invoke-WebRequest -Uri "$concourseUrl$newUri" -WebSession $ciCookie -Method Post -FollowRelLink -Body @{login=$user;password=$pass} -SkipCertificateCheck
+        $req = Invoke-WebRequest -Uri "$concourseUrl$newUri" -WebSession $ciCookie -Method Post -Body @{login=$user;password=$pass} -SkipCertificateCheck
         $req = Invoke-WebRequest -Uri "$concourseUrl/sky/login" -Method Get -SessionVariable ciCookie -SkipCertificateCheck
         Write-Verbose -Message "Received response : $($req | Out-String)"
         Write-Verbose -Message "Received content : $($req.Content | Out-String)"
