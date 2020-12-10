59876206e67e537c5cab19d0e508041f70f06364
Cansın Aldanmaz <cansinaldanmaz@gmail.com>
2020-12-10 11:26:23 +0300

    Uninstall and import concourse before pode starts

--- pode.ps1
+++ pode.ps1
@@ -1,5 +1,7 @@
 Install-Module -Name Pode -Force
 Import-Module -Name Pode
+Get-Module -ListAvailable -Name concourse | Uninstall-Module
+Import-Module ./concourse.psd1 -Force
 Start-PodeServer -Threads 2 {
     New-PodeLoggingMethod -Terminal | Enable-PodeErrorLogging
     New-PodeLoggingMethod -Terminal | Enable-PodeRequestLogging
